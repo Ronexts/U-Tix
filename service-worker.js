235fde7d9722fb497d110517ebdb1abc13ffef05
Ronexts <luthfiassidiq12@gmail.com>
2022-12-28 19:16:24 +0700

    searching way to fix error manifest

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "SW-001";
 const toCache = [
   "/",
   "manifest.json",
-  "assets/js/register.js",
+  "assets/register.js",
   "assets/css/bootstrap.css",
   "assets/css/bootstrap.css.map",
   "assets/css/styles.css",
@@ -24,41 +24,35 @@ self.addEventListener("beforeinstallprompt", (e) => {
   showInstallPromotion();
 });
 
-self.addEventListener("install", function (event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches
-    .open(CACHE_NAME)
-    .then(function (cache) {
-      return cache.addAll(toCache);
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(toCache)
     })
     .then(self.skipWaiting())
-  );
-});
+  )
+})
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match(event.request);
-      });
+        return cache.match(event.request)
+      })
     })
-  );
-});
+  )
+})
 
-self.addEventListener("activate", function (event) {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches
-    .keys()
-    .then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
           if (key !== CACHE_NAME) {
             console.log("[ServiceWorker] Hapus cache lama", key);
-            return caches.delete(key);
+            return caches.delete(key)
           }
         })
-      );
-    })
-    .then(() => self.clients.claim())
+      )
+    }).then(() => self.clients.claim())
   );
 });
